75917844d63b7ce7f0f4779fdccfaa70a5657094
Black Manta <davidlutta2010@gmail.com>
2019-01-14 13:56:25 +0300

    Fix Import

--- ScanDemoExample/scanlibrary/src/main/java/com/scanlibrary/PickImageFragment.java
+++ ScanDemoExample/scanlibrary/src/main/java/com/scanlibrary/PickImageFragment.java
@@ -10,7 +10,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
